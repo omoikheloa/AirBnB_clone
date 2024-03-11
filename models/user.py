b39a8936ee246cd70616f61d7ec9cd157eb0508e
Omo <omoikheloa56@gmail.com>
2024-03-11 10:03:57 -0700

    user update

--- models/user.py
+++ models/user.py
@@ -2,17 +2,16 @@
 """
 Contains User class
 """
-
-
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """User class that inherits from BaseModel"""
+    self.email = ""
+    self.password = ""
+    self.first_name = ""
+    self.last_name = ""
+
     def __init__(self, *args, **kwargs):
-        """Initialization of User instance"""
+        """initializes user"""
         super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
